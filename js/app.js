bd794246c49ba3f5d975314020eb7a4b352be379
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-11-25 18:23:40 +0900

    Create checkmark animation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const onBodyLoadHandler = () => {
-    const preloadArea = document.querySelector('.preload')
-    preloadArea.classList.add('hidden')
-    document.body.classList.remove('still-load')
-}
+  const preloadArea = document.querySelector(".preload");
+  const checkAnimation = document.querySelector(".checkmark")
+  preloadArea.classList.add("hidden");
+  document.body.classList.remove("still-load");
+  checkAnimation.classList.add("on");
+};
